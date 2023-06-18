0265eacd56d07222d7b3c1740e9b97f8a04a3a76
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-06-17 21:36:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var client = new HttpClient();
+
+            var key = "";
+            var city = " Los Angeles ";
+
+            var weatherURL = $"https://openweathermap.org/city/5368361";
+            var response = client.GetAsync(weatherURL).Result;
+        }
+    }
+}
